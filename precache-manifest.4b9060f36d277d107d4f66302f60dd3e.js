7f2a4dc0ed4a27adeca6a688703221c84ceae283
Alexander Corral <acorral@ENGStudios.com>
2019-09-12 12:21:30 -0300

    Updates

---
+++ precache-manifest.4b9060f36d277d107d4f66302f60dd3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d92e64b6db796d5c16ce715f4612625",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "619c194921cf22754ec8",
+    "url": "/monsters-rolodex/static/css/main.225bdcce.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "619c194921cf22754ec8",
+    "url": "/monsters-rolodex/static/js/main.027a1a85.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
